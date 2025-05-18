61d5845812f10a8b389f1e5fae6605bd01d2e901
Barinova Olga <assistancebycompany0.1@gmail.com>
2025-05-18 19:31:16 +0300

    Renamed the files, created a separate entity for сhatRow, added a constraint for the strings, and added validation

---
+++ List/List/ChatsView.swift
@@ -0,0 +1,39 @@
+//
+//  ContentView.swift
+//  List
+//
+//  Created by Olga Dragon on 07.05.2025.
+//
+
+import SwiftUI
+
+struct ChatsView: View {
+    
+    let allChats  = [
+        Chat(name: "Emily", message: "Hey! How’s it going?", time: "10:30", avatar: "girl"),
+        Chat(name: "Mia", message: "Wanna go to the movies?", time: "09:15", avatar: "girl2"),
+        Chat(name: "James", message: "Thanks for your help", time: "Yesterday", avatar: "boy"),
+        Chat(name: "", message: "", time: "", avatar: "boy")
+    ]
+    
+    var chats: [Chat]{
+        allChats.filter{$0.isValid}
+    }
+    
+    var body: some View {
+        
+        NavigationView {
+            List(chats) { chat in
+             ChatRow(chat: chat)
+                
+            }
+            .listStyle(PlainListStyle())
+            .navigationTitle("Chats")
+        }
+        
+    }
+}
+
+#Preview {
+    ChatsView()
+}
