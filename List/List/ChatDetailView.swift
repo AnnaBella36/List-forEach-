c6140673346b91acd72ca84133dd0d212121b137
Barinova Olga <assistancebycompany0.1@gmail.com>
2025-06-09 11:50:33 -0500

    added chat detail view

---
+++ List/List/ChatDetailView.swift
@@ -0,0 +1,48 @@
+//
+//  ChatDetailView.swift
+//  List
+//
+//  Created by Olga Dragon on 09.06.2025.
+//
+
+import SwiftUI
+
+struct ChatDetailView: View {
+    
+    let chat: Chat
+    
+    var body: some View {
+        VStack{
+            ScrollView{
+                Text(chat.message.prefix(2000))
+                    .padding()
+                    .background(Color.blue.opacity(0.1))
+                    .cornerRadius(12)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .padding()
+            }
+            Spacer()
+        }
+        .navigationBarTitleDisplayMode(.inline)
+        .toolbar{
+            ToolbarItem(placement: .principal) {
+                Text(chat.name)
+                    .font(.headline)
+            }
+            ToolbarItem(placement: .navigationBarTrailing) {
+                Image(chat.avatar)
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: 32, height: 32)
+                    .clipShape(Circle())
+            }
+        }
+    }
+}
+
+#Preview {
+    var chat = Chat(name: "Emily", message: "What's up? ", time: "11:00", avatar: "girl")
+    NavigationView {
+        ChatDetailView(chat: chat)
+    }
+}
